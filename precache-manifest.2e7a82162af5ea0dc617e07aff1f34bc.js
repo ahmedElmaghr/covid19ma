74a1e96e44030c98d00af2f2e2887fcd795e06a9
ELMAGHRAOUI <ahmed.2.elmaghraoui.external@atos.net>
2020-03-21 12:49:00 +0100

    Updates

---
+++ precache-manifest.2e7a82162af5ea0dc617e07aff1f34bc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa4b3f279de5c6b86a5d369b169df59",
+    "url": "/covid19ma/index.html"
+  },
+  {
+    "revision": "d0c2aa4c45eadb3ec065",
+    "url": "/covid19ma/static/css/2.a539aa83.chunk.css"
+  },
+  {
+    "revision": "244528e4db49c1067f05",
+    "url": "/covid19ma/static/css/main.53b512be.chunk.css"
+  },
+  {
+    "revision": "d0c2aa4c45eadb3ec065",
+    "url": "/covid19ma/static/js/2.b90026ac.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/covid19ma/static/js/2.b90026ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "244528e4db49c1067f05",
+    "url": "/covid19ma/static/js/main.efdf4842.chunk.js"
+  },
+  {
+    "revision": "97181e2e03d64fe2188e",
+    "url": "/covid19ma/static/js/runtime-main.b9ecd3c7.js"
+  },
+  {
+    "revision": "46239ef0852c21ac2885dc607f695e01",
+    "url": "/covid19ma/static/media/countries.46239ef0.tsv"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/covid19ma/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/covid19ma/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/covid19ma/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/covid19ma/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/covid19ma/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
