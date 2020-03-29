4c7ce24e7e61a701b118e948ae84d48eeb130602
unknown <elmaghraoui.ahmed@gmail.com>
2020-03-29 21:03:02 +0200

    Updates

---
+++ precache-manifest.082dfa132ec3f09db230dd31eed2d1e9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8c488c58c53c78ebae8cdb689a3d1f",
+    "url": "./index.html/index.html"
+  },
+  {
+    "revision": "c5409236e95c361501b6",
+    "url": "./index.html/static/css/2.d1672566.chunk.css"
+  },
+  {
+    "revision": "396e6fc13b510e340387",
+    "url": "./index.html/static/css/main.3f6fad5f.chunk.css"
+  },
+  {
+    "revision": "c5409236e95c361501b6",
+    "url": "./index.html/static/js/2.2626a265.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./index.html/static/js/2.2626a265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "396e6fc13b510e340387",
+    "url": "./index.html/static/js/main.3b263561.chunk.js"
+  },
+  {
+    "revision": "97d3f133cd2626901ba5",
+    "url": "./index.html/static/js/runtime-main.c0559e23.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./index.html/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./index.html/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./index.html/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./index.html/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./index.html/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./build/precache-manifest.0ce0d24c1b2e6591f2a1e71d005ed38c.js"
+  "./index.html/precache-manifest.082dfa132ec3f09db230dd31eed2d1e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
